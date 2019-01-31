eeac92e5b71cd5f1a06bc686b01a5526fb57b363
felixgondwe <gondwefelix8@gmail.com>
2019-01-31 15:02:42 -0800

    debug remote

--- src/curation_views.py
+++ src/curation_views.py
@@ -309,12 +309,11 @@ def db_sign_in(request):
         session = request.session
         session['email'] = curator.email
         session['username'] = curator.username
-        test_msg = 'User ' + curator.email + ' was successfuly authenticated.'
         log.info('User ' + curator.email + ' was successfuly authenticated.')
         return { 'username': session['username'] }
     except Exception as e:
         traceback.print_exc()
-        return HTTPForbidden(body=json.dumps({'error': 'Incorrect login details. ' + str(e) + test_msg}))
+        return HTTPForbidden(body=json.dumps({'error': 'Incorrect login details. ' + str(e)}))
     finally:
         if Temp_session:
             Temp_session.close()
