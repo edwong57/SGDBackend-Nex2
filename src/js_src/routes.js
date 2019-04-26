79a8e93d770b8905269ab0f9a952da4353959eca
sweng66 <sweng@stanford.edu>
2019-04-26 12:00:39 -0700

    testing

--- src/js_src/routes.js
+++ src/js_src/routes.js
@@ -36,8 +36,7 @@ import ColleaguesIndex from './containers/colleagues/index';
 import ColleaguesShow from './containers/colleagues/show';
 // import AuthorResponse from './containers/authorResponse/index';
 import NewColleague from './containers/colleagues/new';
-// import AuthorResponse from './containers/authorResponse/new';
-import AuthorResponse from './containers/colleagues/new';
+import AuthorResponse from './containers/authorResponse/new';
 
 export default (
   <Route>
