9af11cc7a441b5a7884caf10fdb0621e9aa24ddb
sweng66 <sweng@stanford.edu>
2019-05-01 13:03:13 -0700

    testing

--- src/js_src/containers/reserve/authorResponse.js
+++ src/js_src/containers/reserve/authorResponse.js
@@ -5,30 +5,32 @@ import AuthorResponseForm from '../../components/authorResponseForm';
 import Loader from '../../components/loader';
 
 class AuthorResponse extends Component {
+
+  // selectorValue: null, 
   constructor(props) {
     super(props);
     this.state = {
       data: null,
       pmid: null,
       citation: null,
-      hasFunction: false,
-      hasDataset: false,
-      function: null,
-      dataset: null,
-      genes: null,
-      other: null,
-      selectorValue: null,
+      email: null,
+      has_novel_data: false,
+      has_large_scale_data: false,
+      novel_research_result: null,
+      dataset_desc: null,
+      gene_list: null,
+      other_desc: null,
       formValue: null,
       isPending: false
     };
   }
 
-  handleHasFunctionChange() {
-    this.setState({ hasFunction: !this.state.hasFunction });
+  handleHasNovelDataChange() {
+    this.setState({ has_novel_data: !this.state.has_novel_data });
   }
 
-  handleHasDatasetChange() {
-    this.setState({ hasDataset: !this.state.hasDataset });
+  handleHasLargeScaleDataChange() {
+    this.setState({ has_large_scale_data: !this.state.has_large_scale_data });
   }
 
   renderSelector() {
@@ -41,13 +43,13 @@ class AuthorResponse extends Component {
           <div className='large-12 columns'>
                <p>Pubmed ID for your paper <font color='red'>(Required)</font>: <textarea ref='pmid' value={this.state.pmid} name='pmid' rows='1' cols='50'></textarea></p>
                <p>Citation for your paper: <textarea ref='citation' value={this.state.citation} name='citation' rows='1' cols='50'></textarea></p>
-               <p>Your E-mail address <font color='red'>(Required)</font>: <textarea ref='email' value={this.state.email} name='pmid' rows='1' cols='50'></textarea></p>
-               <p>Does this paper contain novel characterizations of the function, role, or localization of a gene product(s)? <input checked={this.state.hasFunction} onChange={this.handleHasFunctionChange.bind(this)} type='checkbox' /> Yes<br></br>
-               If yes, please summarize briefly the novel results. <textarea ref='function' value={this.state.function} name='function' rows='3' cols='50'></textarea></p>
-               <p>If this paper focuses on specific genes/proteins, please identify them here (enter a list of gene names/systematic names): <textarea ref='genes' value={this.state.genes} name='genes' rows='1' cols='50'></textarea></p>
-               <p>Does this study include large-scale datasets that you would like to see incorporated into SGD? <input checked={this.state.hasDataset} onChange={this.handleHasDatasetChange.bind(this)} type='checkbox' /> Yes<br></br>
-               If yes, please describe briefly the type(s) of data. <textarea ref='dataset' value={this.state.dataset} name='dataset' rows='3' cols='50'></textarea></p> 
-               <p>Is there anything else that you would like us to know about this paper? <textarea ref='other' value={this.state.other} name='other' rows='3' cols='50'></textarea></p>
+               <p>Your E-mail address <font color='red'>(Required)</font>: <textarea ref='email' value={this.state.email} name='email' rows='1' cols='50'></textarea></p>
+               <p>Does this paper contain novel characterizations of the function, role, or localization of a gene product(s)? <input checked={this.state.has_novel_data} onChange={this.handleHasNovelDataChange.bind(this)} type='checkbox' /> Yes<br></br>
+               If yes, please summarize briefly the novel results. <textarea ref='novel_research_result' value={this.state.novel_research_result} name='novel_research_result' rows='3' cols='50'></textarea></p>
+               <p>If this paper focuses on specific genes/proteins, please identify them here (enter a list of gene names/systematic names): <textarea ref='gene_list' value={this.state.gene_list} name='gene_list' rows='1' cols='50'></textarea></p>
+               <p>Does this study include large-scale datasets that you would like to see incorporated into SGD? <input checked={this.state.has_large_scale_data} onChange={this.handleHasLargeScaleDataChange.bind(this)} type='checkbox' /> Yes<br></br>
+               If yes, please describe briefly the type(s) of data. <textarea ref='dataset_desc' value={this.state.dataset_desc} name='dataset_desc' rows='3' cols='50'></textarea></p> 
+               <p>Is there anything else that you would like us to know about this paper? <textarea ref='other_desc' value={this.state.other_desc} name='other_desc' rows='3' cols='50'></textarea></p>
                <p><input type="submit" value="Submit" className="button secondary"></input></p>
           </div>
         </div>
@@ -57,7 +59,7 @@ class AuthorResponse extends Component {
 
   renderForm() {
     if (this.state.isPending) return <Loader />;
-    if (!this.state.selectorValue) return null;
+    // if (!this.state.selectorValue) return null;
     let _requestMethod = 'POST';
     let url = 'authorResponse';
     let _submitText = this.props.submitText || 'Next';
