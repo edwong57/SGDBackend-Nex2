437ac502cf7d5d142c6deed67b094567ac206b57
sweng66 <sweng@stanford.edu>
2019-11-25 23:01:41 -0800

    testing

--- src/author_response_helpers.py
+++ src/author_response_helpers.py
@@ -23,32 +23,30 @@ def insert_author_response(request):
 
         has_novel_research = request.params.get('has_novel_research')
         
-        return HTTPBadRequest(body=json.dumps({'error': "has_novel_research="+str(has_novel_research)}), content_type='text/json')
+        has_large_scale_data = request.params.get('has_large_scale_data')
 
-        # has_large_scale_data = request.params.get('has_large_scale_data')
-
-        # research_results = request.params.get('research_result')
+        research_results = request.params.get('research_result')
          
-        # dataset_description = request.params.get('dataset_desc')
+        dataset_description = request.params.get('dataset_desc')
 
-        # gene_list = request.params.get('gene_list')
+        gene_list = request.params.get('gene_list')
 
-        # other_description = request.params.get('other_desc')
+        other_description = request.params.get('other_desc')
 
-        # x = Authorresponse(source_id = source_id,
-        #                   pmid = pmid,
-        #                   author_email = email,
-        #                   has_novel_research = str(has_novel_research),
-        #                   has_large_scale_data = str(has_large_scale_data),
-        #                   has_fast_track_tag = '0',
-        #                   curator_checked_datasets = '0',
-        #                   curator_checked_genelist = '0',
-        #                   no_action_required = '0',
-        #                   research_results = research_results,
-        #                   gene_list = gene_list,
-        #                   dataset_description = dataset_description,
-        #                   other_description = other_description,
-        #                   created_by = created_by)
+        x = Authorresponse(source_id = source_id,
+                           pmid = pmid,
+                           author_email = email,
+                           has_novel_research = str(has_novel_research),
+                           has_large_scale_data = str(has_large_scale_data),
+                           has_fast_track_tag = '0',
+                           curator_checked_datasets = '0',
+                           curator_checked_genelist = '0',
+                           no_action_required = '0',
+                           research_results = research_results,
+                           gene_list = gene_list,
+                           dataset_description = dataset_description,
+                           other_description = other_description,
+                           created_by = created_by)
 
         DBSession.add(x)
         transaction.commit()
