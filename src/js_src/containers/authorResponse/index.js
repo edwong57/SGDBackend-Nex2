2e8fd12826c6df1237b04f5ca4fee10511100d80
sweng66 <sweng@stanford.edu>
2019-11-26 13:01:48 -0800

    testing

--- src/js_src/containers/authorResponse/index.js
+++ src/js_src/containers/authorResponse/index.js
@@ -83,7 +83,7 @@ class AuthorResponse extends Component {
           <label>Citation</label>
           <input name='citation' value={this.props.authorResponse.citation} onChange={this.onChange} type='text' />
           <p>Does this paper contain novel characterizations of the function, role, or localization of a gene product(s)?
-          Yes <input type="checkbox" name='has_novel_research' value='1' onChange={this.onChange} />  
+          Yes <input type="checkbox" name='has_novel_research' value='1' onChange={this.onChange} /><br></br> 
           If yes, please summarize briefly the novel results.</p>
           <input name='research_result' value={this.props.authorResponse.research_result} onChange={this.onChange} type='text' />
           <p>If this paper focuses on specific genes/proteins, please identify them here (enter a list of gene names/systematic names).</p>
