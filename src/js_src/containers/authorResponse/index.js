d4ce0c62f1104d2728e626de57173741570635c4
sweng66 <sweng@stanford.edu>
2019-11-26 08:50:06 -0800

    testing

--- src/js_src/containers/authorResponse/index.js
+++ src/js_src/containers/authorResponse/index.js
@@ -3,22 +3,22 @@ import React, { Component } from 'react';
 import AuthorResponse from './authorResponse';
 
 class SubmitData extends Component {
-    constructor(props) {
-	super(props);
-	this.state = {
-	    isComplete: false
-	};
-    }
-    handleColleagueCompletion() {
-	this.setState({ isComplete: true });
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      isComplete: false
+    };
+  }
+  handleColleagueCompletion() {
+    this.setState({ isComplete: true });
+  }
 
-    render() {
-	if (this.state.isComplete) {
-	    return <h2 style={{ marginTop: '3rem' }}>Thanks for your update! SGD curators will review.</h2>;
-	}
-	return <AuthorResponse onComplete={this.handleColleagueCompletion.bind(this)} />;
+  render() {
+    if (this.state.isComplete) {
+      return <h2 style={{ marginTop: '3rem' }}>Thanks for your update! SGD curators will review.</h2>;
     }
+    return <AuthorResponse onComplete={this.handleColleagueCompletion.bind(this)} />;
+  }
 }
 
 export default SubmitData;
