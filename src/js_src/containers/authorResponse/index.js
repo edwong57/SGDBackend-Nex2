fbf9d3009c53d3e0ba407e447a0b2b154d0595b2
sweng66 <sweng@stanford.edu>
2019-11-25 22:40:34 -0800

    testing

--- src/js_src/containers/authorResponse/index.js
+++ src/js_src/containers/authorResponse/index.js
@@ -5,7 +5,7 @@ import { setData } from '../../actions/authorResponseActions';
 import { connect } from 'react-redux';
 import { setError, setMessage } from '../../actions/metaActions';
 // const ADD_DATA = '/add_author_response';
-const ADD_DATA = '/add_author_response';  
+const ADD_DATA = '/phenotype_add';  
 
 class AuthorResponse extends Component {
 
