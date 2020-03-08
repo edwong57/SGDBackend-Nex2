03393547505209260f4c00df47568e5032b57b38
Shuai Weng <sweng@stanford.edu>
2020-03-07 19:22:21 -0800

    fixed a bug

--- src/variant_helpers.py
+++ src/variant_helpers.py
@@ -121,7 +121,7 @@ def get_variant_data(request):
                             "end": x.end_index,
                             "score": x.score,
                             "variant_type": x.variant_type }
-            if x.variant_type not in ['Insertion', 'Deletion']:
+            if x.variant_type not in ['Insertion', 'Deletion'] and x.variant_type is not None:
                 protein_row['snp_type'] = x.snp_type.capitalize()
             variant_protein.append(protein_row)
     data['variant_data_dna'] = variant_dna
