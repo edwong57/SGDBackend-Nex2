5d6462a733b61a3e3ddcc7727fdba88020897f66
Shuai Weng <sweng@stanford.edu>
2020-04-20 16:34:52 -0700

    testing

--- src/phenotype_helpers.py
+++ src/phenotype_helpers.py
@@ -298,23 +298,29 @@ def add_phenotype_conditions(curator_session, CREATED_BY, annotation_id, group_i
 
 def check_gene_names(genes, dbentity_id_list, dbentity_id_to_gene_name):
 
+    gene_for_allele = None
     for gene in genes:
         if gene.startswith('SGD:S') or gene.startswith('S0'):
             sgdid = gene.replace('SGD:', '')
             dbentity = DBSession.query(Locusdbentity).filter_by(sgdid=sgdid).one_or_none()
             if dbentity is not None:
                 dbentity_id_list.append(dbentity.dbentity_id)
-                dbentity_id_to_gene_name[dbentity.dbentity_id] = dbentity.gene_name
+                if dbentity.gene_name:
+                    dbentity_id_to_gene_name[dbentity.dbentity_id] = dbentity.gene_name
+                else:
+                    dbentity_id_to_gene_name[dbentity.dbentity_id] = dbentity.systematic_name
             else:
                 return HTTPBadRequest(body=json.dumps({'error': "The SGDID " + sgdid + " is not in the database or it is not for a gene."}), content_type='text/json')
         else:
             dbentity = DBSession.query(Locusdbentity).filter_by(systematic_name=gene).one_or_none()
             if dbentity is not None:
                 dbentity_id_list.append(dbentity.dbentity_id)
-                dbentity_id_to_gene_name[dbentity.dbentity_id] = dbentity.gene_name
+                if dbentity.gene_name:
+                    dbentity_id_to_gene_name[dbentity.dbentity_id] = dbentity.gene_name
+                else:
+                    dbentity_id_to_gene_name[dbentity.dbentity_id] = dbentity.systematic_name
             else:
                 return HTTPBadRequest(body=json.dumps({'error': "The systematic_name " + gene + " is not in the database."}), content_type='text/json')
-        
 
 def check_reference(ref_id):
 
@@ -970,25 +976,13 @@ def update_phenotype_annotations(request):
         sgd = DBSession.query(Source).filter_by(display_name='SGD').one_or_none()
         source_id = sgd.source_id
 
-        
         gene_id_list = request.params.get('gene_id_list', '')
 
-
-
-        
-        return HTTPBadRequest(body=json.dumps({'error': "gene_id_list=" + gene_id_list}), content_type='text/json')
-
-
-
-
-    
-    
         if gene_id_list == '':
             return HTTPBadRequest(body=json.dumps({'error': "Please choose one or more genes from the pulldown menu."}), content_type='text/json')
 
         gene_ids = gene_id_list.split(' ')
 
-        
         annotation_ids = []
         annotation_id_to_gene = {}
         gene_name = None
@@ -1121,7 +1115,7 @@ def update_phenotype_annotations(request):
             paUpdatedCols.append('details')
 
         ## allele
-        allele = request.params.get('allele_id', '')        
+        allele = request.params.get('allele_id', '')
         if allele == 'null':
             allele = ''
         allele_id = None
