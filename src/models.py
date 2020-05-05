10a0a382546ab3a6d79db8ab6da21806f916f671
Shuai Weng <sweng@stanford.edu>
2020-05-04 19:56:30 -0700

    fixed to make the non-gene tags work

--- src/models.py
+++ src/models.py
@@ -2446,12 +2446,6 @@ class Referencedbentity(Dbentity):
                 comments.append(comment)
             tag2comments[tag] = comments
             
-
-            
-        return tag2dbentityNames
-
-    
-            
         tag_list = []    
         for tag in tag2dbentityNames:
             dbentity_names = tag2dbentityNames[tag]
