02f7c0d0443bc1b5136d14039d74802f581872d1
Shuai Weng <sweng@stanford.edu>
2020-05-20 13:57:18 -0700

    fixed session issue

--- src/curation_views.py
+++ src/curation_views.py
@@ -1084,14 +1084,15 @@ def add_new_colleague_triage(request):
             created_by = 'OTTO'
         )
         curator_session.add(new_colleague)
+        curator_session.flush()
         curator_session.commit()
         return {'colleague_id': new_colleague.colleague_id}
     except IntegrityError as IE:
-        curator_session.abort()
+        curator_session.rollback()
         log.error(IE)
         return HTTPBadRequest(body=json.dumps({'message': 'Orcid or Email already exists, if error persist Please contact sgd-helpdesk@lists.stanford.edu'}), content_type='text/json')
     except Exception as e:
-        curator_session.abort()
+        curator_session.rollback()
         log.error(e)
         return HTTPBadRequest(body=json.dumps({'message': str(e) + ' something bad happened'}), content_type='text/json')
 
