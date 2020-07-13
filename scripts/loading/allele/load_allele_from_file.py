2ed8bf0cbe261ac56a5668397389e3b1f33d2880
Shuai Weng <sweng@stanford.edu>
2020-07-13 09:33:33 -0700

    testing

--- scripts/loading/allele/load_allele_from_file.py
+++ scripts/loading/allele/load_allele_from_file.py
@@ -3,7 +3,8 @@ import os
 from datetime import datetime
 import sys
 from src.models import Source, So, Dbentity, Locusdbentity, Referencedbentity, Alleledbentity,\
-                       LocusAllele, AlleleReference, AlleleAlias, AllelealiasReference
+                       AlleleReference, LocusAllele, LocusalleleReference, AlleleAlias, \
+                       AllelealiasReference
 from scripts.loading.database_session import get_session
 
 __author__ = 'sweng66'
@@ -25,8 +26,22 @@ def load_data(infile):
     source = nex_session.query(Source).filter_by(display_name='SGD').one_or_none()
     source_id = source.source_id
     so_to_id =  dict([(x.display_name, x.so_id) for x in nex_session.query(So).all()])
+
     gene_to_locus_id = dict([(x.gene_name, x.dbentity_id) for x in nex_session.query(Locusdbentity).all()])
+
     pmid_to_reference_id = dict([(x.pmid, x.dbentity_id) for x in nex_session.query(Referencedbentity).all()])
+
+    allele_to_id = dict([(x.display_name.upper(), x.dbentity_id) for x in nex_session.query(Dbentity).filter_by(subclass='ALLELE').all()])
+
+    allele_reference_to_id = dict([((x.allele_id, x.reference_id), x.allele_reference_id) for x in nex_session.query(AlleleReference).all()])
+
+    locus_allele_to_id = dict([((x.locus_id, x.allele_id), x.locus_allele_id) for x in nex_session.query(LocusAllele).all()])
+
+    locus_allele_reference_to_id = dict([((x.locus_allele_id, x.reference_id), x.locusallele_reference_id) for x in nex_session.query(LocusalleleReference).all()])
+
+    allele_alias_to_id = dict([((x.allele_id, x.display_name.upper()), x.allele_alias_id) for x in nex_session.query(AlleleAlias).all()])
+
+    allele_alias_reference_to_id = dict([((x.allele_alias_id, x.reference_id), x.allelealias_reference_id) for x in nex_session.query(AllelealiasReference).all()])
     
     f = open(infile)
     
@@ -34,11 +49,12 @@ def load_data(infile):
     
     for line in f:
         pieces = line.strip().split("\t")
-        log.info("adding alleledbentiy: " + x.display_name + "...")
+        if pieces[0] in ['PMID', 'pmid']:
+            continue
         if pieces[0] == '':
             log.info("Missing PMID for line: " + line)
             continue
-        reference_id = pmid_to_reference_id(int(pieces[0]))
+        reference_id = pmid_to_reference_id.get(int(pieces[0]))
         if reference_id is None:
             log.info("The PMID: " + pieces[1] + " is not in the database")
             continue
@@ -61,37 +77,59 @@ def load_data(infile):
             log.info("Missing created_by for line: " + line)
             continue
 
-        log.info("loading data into dbentity/alleledbentity table...")
+        count = count + 1
         
-        allele_id = insert_alleledbentity(nex_session, allele_name.replace(" ", "_"), allele_name, allele_desc,
-                                          source_id, so_id, created_by)
+        allele_id = allele_to_id.get(allele_name.upper())
 
-        log.info("loading data into locus_allele table...")
-        locus_allele_id = insert_locus_allele(nex_session, locus_id, allele_id, source_id, created_by)
+        if allele_id is None:
+            log.info("loading data into dbentity/alleledbentity table...")
+            allele_id = insert_alleledbentity(nex_session, allele_name.replace(" ", "_"), allele_name, allele_desc,
+                                              source_id, so_id, created_by)
 
-        log.info("loading data into locusallele_reference table...")
-        insert_locusallele_reference(nex_session, reference_id, locus_allele_id, source_id, created_by)
+        if (allele_id, reference_id) not in allele_reference_to_id:
+            log.info("loading data into allele_reference table...")
+            insert_allele_reference(nex_session, allele_id, reference_id, source_id, created_by)
+            
+        locus_allele_id = locus_allele_to_id.get((locus_id, allele_id))
+        
+        if locus_allele_id is None:
+            log.info("loading data into locus_allele table...")
+            locus_allele_id = insert_locus_allele(nex_session, locus_id, allele_id, source_id, created_by)
+
+        if (locus_allele_id, reference_id) not in locus_allele_reference_to_id:
+            log.info("loading data into locusallele_reference table...")
+            insert_locusallele_reference(nex_session, reference_id, locus_allele_id, source_id, created_by)
 
         if alias:
-            log.info("loading data into allele_alias table...")
-            allele_alias_id = insert_allele_alias(nex_session, allele_id, alias, source_id, created_by)
+            allele_alias_id = allele_alias_to_id.get((allele_id, alias.upper()))
+            if allele_alias_id is None:
+                log.info("loading data into allele_alias table...")
+                allele_alias_id = insert_allele_alias(nex_session, allele_id, alias, source_id, created_by)
 
-            log.info("loading data into allelealias_reference table...")
-            insert_allelealias_reference(nex_session, reference_id, allele_aliase_id, source_id, created_by)
+            if (allele_alias_id, reference_id) not in allele_alias_reference_to_id:
+                log.info("loading data into allelealias_reference table...")
+                insert_allelealias_reference(nex_session, reference_id, allele_alias_id, source_id, created_by)
             
         if count >= 300:
-            nex_session.rollback()  
-            # nex_session.commit()
+            # nex_session.rollback()  
+            nex_session.commit()
             count = 0
                         
-    nex_session.rollback()
-    # nex_session.commit()
+    # nex_session.rollback()
+    nex_session.commit()
         
     nex_session.close()
     log.info("Done!")
     log.info(str(datetime.now()))
 
 
+def insert_allele_reference(nex_session, allele_id, reference_id, source_id, created_by):
+
+    x = AlleleReference(allele_id = allele_id,
+                        reference_id = reference_id,
+                        source_id = source_id,
+                        created_by = created_by)
+    nex_session.add(x)
 
 def insert_locus_allele(nex_session, locus_id, allele_id, source_id, created_by):
 
@@ -126,10 +164,10 @@ def insert_allele_alias(nex_session, allele_id, alias, source_id, created_by):
     nex_session.refresh(x)
     return x.allele_alias_id
 
-def insert_allelealias_reference(nex_session, reference_id, allele_aliase_id, source_id, created_by):
+def insert_allelealias_reference(nex_session, reference_id, allele_alias_id, source_id, created_by):
 
     x = AllelealiasReference(reference_id = reference_id,
-                             allele_alias_id = allele_aliase_id,
+                             allele_alias_id = allele_alias_id,
                              source_id = source_id,
                              created_by = created_by)
     nex_session.add(x)
@@ -142,7 +180,6 @@ def insert_alleledbentity(nex_session, format_name, display_name, desc, source_i
                        subclass = 'ALLELE',
                        dbentity_status = 'Active',
                        created_by = created_by,
-                       date_created = date_created,
                        description = desc,
                        so_id = so_id)
     
