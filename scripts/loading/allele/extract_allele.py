1fb379b9da830ec3f7fc307fb82fc2714425e5c4
Shuai Weng <sweng@stanford.edu>
2020-07-20 20:01:55 -0700

    testing

--- scripts/loading/allele/extract_allele.py
+++ scripts/loading/allele/extract_allele.py
@@ -1,4 +1,4 @@
-from src.models import Locusdbentity, Dbentity, Geninteractionannotation
+from src.models import Locusdbentity, Dbentity, Geninteractionannotation, Referencedbentity
 from scripts.loading.database_session import get_session
 
 __author__ = 'sweng66'
@@ -9,8 +9,16 @@ locus_id_to_names =  dict([(x.dbentity_id, (x.systematic_name, x.gene_name)) for
 
 allele_to_id = dict([(x.display_name.upper(), x.dbentity_id) for x in nex_session.query(Dbentity).filter_by(subclass='ALLELE').all()])
 
+reference_id_to_pmid =  dict([(x.dbentity_id, x.pmid) for x in nex_session.query(Referencedbentity).all()])
+
 all = nex_session.query(Geninteractionannotation).filter(Geninteractionannotation.description.like('%allele%')).all()
 
+allele_to_skip = {}
+f = open("scripts/loading/allele/data/genInteractionAlleles2NOTload072020.tsv")
+for line in f:
+    allele_to_skip[line.strip().upper()] = 1
+f.close()
+    
 for x in all:
     (orf, gene) = locus_id_to_names.get(x.dbentity1_id)
     (orf2, gene2) = locus_id_to_names.get(x.dbentity2_id)
@@ -28,9 +36,13 @@ for x in all:
             word = word[0:-2]
         if word[0] in ['(', '/', '|']:
             word = word[1:]
+
         if word in found:
             continue
         found[word] = 1
+        if  word.upper() in allele_to_skip:
+            continue
+        
         if word.upper() not in [orf.upper(), gene.upper(), orf2.upper(), gene2.upper()]:
             allele = word
             matching_gene = None
@@ -43,7 +55,8 @@ for x in all:
             elif word.upper().startswith(gene2.upper()):
                 matching_gene = gene2
             if matching_gene is not None:
-                print (allele + "\t" + matching_gene + "\t" + str(x.annotation_id) + "\t" + gene + "/" + orf + "\t" + gene2 + "/" + orf2 + "\t" + x.description)
+                pmid = reference_id_to_pmid.get(x.reference_id)
+                print (allele + "\t" + matching_gene + "\t" + str(x.annotation_id) + "\t" + gene + "/" + orf + "\t" + gene2 + "/" + orf2 + "\t" + str(pmid) + "\t" + x.description + "\t" + str(x.date_created).split(' ')[0])
                 
 nex_session.close()
 
