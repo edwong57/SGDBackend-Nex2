752f8384b09b39664bdca96dd15a9de2e0b0f7d1
Shuai Weng <sweng@stanford.edu>
2020-07-21 13:50:39 -0700

    fixed so it will clean up rows in curatoractivity

--- src/curation_views.py
+++ src/curation_views.py
@@ -2950,7 +2950,7 @@ def transfer_delete_reference_annotations(request):
                         transfer_delete_method(Proteinsequenceannotation)
                     elif(table == 'Regulationannotation'):
                         transfer_delete_method(Regulationannotation)
-                    
+                                            
                 transaction.commit()
                 
                 sgd_id = request.matchdict['id']
@@ -2979,8 +2979,13 @@ def transfer_delete_reference_annotations(request):
 def delete_reference(request):
 
     def delete_helper(table,table_name):
-        count = curator_session.query(table).filter_by(reference_id = reference.dbentity_id).count()
-        curator_session.query(table).filter_by(reference_id = reference.dbentity_id).delete()
+        count = 0
+        if table == 'CuratorActivity':
+            count = curator_session.query(table).filter_by(dbentity_id = reference.dbentity_id).count()
+            curator_session.query(table).filter_by(dbentity_id = reference.dbentity_id).delete()
+        else:
+            count = curator_session.query(table).filter_by(reference_id = reference.dbentity_id).count()
+            curator_session.query(table).filter_by(reference_id = reference.dbentity_id).delete()
         log.info('{} records being deleted from {} for reference_id {}'.format(count,table_name,sgd_id))
 
     if not check_csrf_token(request, raises=False):
@@ -3036,7 +3041,7 @@ def delete_reference(request):
                         delete_helper(Referenceunlink,table_name)
                     elif table_name == 'ReferenceFile':
                         delete_helper(ReferenceFile,table_name)
-                    elif table_name == 'CuratorActivity':
+                    else:
                         delete_helper(CuratorActivity,table_name)
                     
                 
