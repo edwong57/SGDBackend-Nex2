4cd1aad2fb4b9bdbf32669cae1532868fa0e4208
Shuai Weng <sweng@stanford.edu>
2020-08-14 22:46:16 -0700

    adding code to get gsa scores / p-values

--- scripts/loading/allele/extract_allele.py
+++ scripts/loading/allele/extract_allele.py
@@ -11,7 +11,7 @@ allele_to_id = dict([(x.display_name.upper(), x.dbentity_id) for x in nex_sessio
 
 reference_id_to_pmid =  dict([(x.dbentity_id, x.pmid) for x in nex_session.query(Referencedbentity).all()])
 
-all = nex_session.query(Geninteractionannotation).filter(Geninteractionannotation.description.like('%allele%')).all()
+all = nex_session.query(Geninteractionannotation).filter(Geninteractionannotation.description.like('%|alleles:%')).filter(Geninteractionannotation.description.like('%[SGA score%')).all()
 
 allele_to_skip = {}
 f = open("scripts/loading/allele/data/genInteractionAlleles2NOTload072020.tsv")
@@ -26,37 +26,25 @@ for x in all:
         gene = orf
     if gene2 is None:
         gene2 = orf2
-        
-    words = x.description.split(' ')
-    found = {}
-    for word in words:
-        if len(word) < 4:
-            continue
-        if word[-1] in [')', ',', '/', '|', ';', ':', '.']:
-            word = word[0:-2]
-        if word[0] in ['(', '/', '|']:
-            word = word[1:]
+      
+    desc_list = x.description.split('|')
+    for desc in desc_list[1:]:
+        if desc.startswith('alleles: '):
+            pieces = desc.replace("alleles: ", "").split('[SGA score = ')
+            alleles = pieces[0].split(' - ')
+            scores = pieces[1].replace("]", "").replace(" P-value = ", "").split(',')
+            sga_score = scores[0]
+            pvalue = scores[1]
+            if float(sga_score) < 0.16 or float(pvalue) >= 0.05:
+                continue
+            if alleles[0] in allele_to_skip or alleles[1] in allele_to_skip:
+                continue
+            if alleles[0] in [orf.upper(), gene.upper(), orf2.upper(), gene2.upper()] and alleles[1] in [orf.upper(), gene.upper(), orf2.upper(), gene2.upper()]:
+                continue
 
-        if word in found:
-            continue
-        found[word] = 1
-        if  word.upper() in allele_to_skip:
-            continue
-        
-        if word.upper() not in [orf.upper(), gene.upper(), orf2.upper(), gene2.upper()]:
-            allele = word
-            matching_gene = None
-            if word.upper().startswith(orf.upper()):
-                matching_gene = orf
-            elif word.upper().startswith(gene.upper()):
-                matching_gene =	gene
-            elif word.upper().startswith(orf2.upper()):
-                matching_gene = orf2
-            elif word.upper().startswith(gene2.upper()):
-                matching_gene = gene2
-            if matching_gene is not None:
-                pmid = reference_id_to_pmid.get(x.reference_id)
-                print (allele + "\t" + matching_gene + "\t" + str(x.annotation_id) + "\t" + gene + "/" + orf + "\t" + gene2 + "/" + orf2 + "\t" + str(pmid) + "\t" + x.description + "\t" + str(x.date_created).split(' ')[0])
+            pmid = reference_id_to_pmid.get(x.reference_id)
+
+            print (alleles[0] + "\t" + alleles[1]+ "\t" + str(x.annotation_id) + "\t" + gene + "/" + orf + "\t" + gene2 + "/" + orf2 + "\t" + str(x.reference_id) + "\t" + str(pmid) + "\t" + str(sga_score) + "\t" + str(pvalue) + "\t" + x.description + "\t" + str(x.date_created).split(' ')[0])
                 
 nex_session.close()
 
