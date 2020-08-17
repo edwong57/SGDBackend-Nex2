d76773ccc175cc9fa2f34fa44e0f64c90ca08922
Shuai Weng <sweng@stanford.edu>
2020-08-17 11:38:59 -0700

    testing

--- scripts/loading/allele/extract_allele.py
+++ scripts/loading/allele/extract_allele.py
@@ -1,9 +1,11 @@
-from src.models import Locusdbentity, Dbentity, Geninteractionannotation, Referencedbentity
+from src.models import Locusdbentity, Dbentity, Geninteractionannotation, Referencedbentity, LocusAlias
 from scripts.loading.database_session import get_session
 
 __author__ = 'sweng66'
 
 nex_session = get_session()
+
+alias_to_locus_id = dict([(x.display_name.upper(), x.locus_id) for x in nex_session.query(LocusAlias).all()])
 
 locus_id_to_names =  dict([(x.dbentity_id, (x.systematic_name, x.gene_name)) for x in nex_session.query(Locusdbentity).all()])
 
@@ -38,7 +40,15 @@ for x in all:
             allele1 = alleles[0].strip()
             allele2 = alleles[1].strip()
             
-            if float(sga_score) < 0.16 or float(pvalue) >= 0.05:
+            if float(pvalue) >= 0.05:
+                continue
+            
+            type = None
+            if float(sga_score) > 0.16:
+                type = 'positive'
+            elif float(sga_score) < -0.12:
+                type = 'negative'
+            if type is None:
                 continue
             
             if allele1 in allele_to_skip:
@@ -49,24 +59,56 @@ for x in all:
                 continue
 
             if allele1 is not None:
-                if allele1.upper() == gene.upper() or allele1.upper() == gene2.upper():
+                if allele1 == gene.upper() or allele1.upper() == gene2.upper():
+                # if allele1 in [gene.upper(), gene2.upper(), orf.upper(), orf2.upper()]:
                     allele1 = None
                 elif not allele1.upper().startswith(gene.upper()) and not allele1.upper().startswith(gene2.upper()):
-                    allele1 = None
-                    
+                    pieces = allele1.split('-')
+                    if len(pieces) == 1:
+                        allele1 = None
+                    else:
+                        locus_id = alias_to_locus_id.get(pieces[0].upper())
+                        if locus_id is None:
+                            allele1 = None
+                        else:
+                            (thisOrf, thisGene) = locus_id_to_names.get(locus_id)
+                            if thisOrf == orf:
+                                allele1 = gene.lower() + "-(" + allele1 + ")"
+                            elif thisOrf == orf2:
+                                allele1 = gene2.lower() + "-(" + allele1 + ")"
+                            else:
+                                allele1 = None
             if allele2 is not None:
                 if allele2.upper() == gene.upper() or allele2.upper() == gene2.upper():
+                # if allele2 in [gene.upper(), gene2.upper(), orf.upper(), orf2.upper()]:
                     allele2 = None
                 elif not allele2.upper().startswith(gene.upper()) and not allele2.upper().startswith(gene2.upper()):
-                    allele2 = None
+                    pieces = allele2.split('-')
+                    if len(pieces) == 1:
+                        allele2 = None
+                    else:
+                        locus_id = alias_to_locus_id.get(pieces[0].upper())
+                        if locus_id is None:
+                            allele2 = None
+                        else:
+                            (thisOrf, thisGene) = locus_id_to_names.get(locus_id)
+                            if thisOrf == orf:
+                                allele2 = gene.lower() + "-(" + allele2 + ")"
+                            elif thisOrf == orf2:
+                                allele2 = gene2.lower() + "-(" + allele2 + ")"
+                            else:
+                                allele2 = None
                                     
-            if allele1 is None or allele2 is None:
-            # if allele1 is None and allele2 is None: 
+            # if allele1 is None or allele2 is None:
+            if allele1 is None and allele2 is None: 
                 continue
 
+            if allele1 is None:
+                (allele1, allele2) = (allele2, allele1)
+                
             pmid = reference_id_to_pmid.get(x.reference_id)
 
-            print (str(allele1) + "\t" + str(allele2) + "\t" + str(x.annotation_id) + "\t" + gene + "/" + orf + "\t" + gene2 + "/" + orf2 + "\t" + str(x.reference_id) + "\t" + str(pmid) + "\t" + str(sga_score) + "\t" + str(pvalue) + "\t" + x.description + "\t" + str(x.date_created).split(' ')[0])
+            print (str(allele1) + "\t" + str(allele2) + "\t" + str(x.annotation_id) + "\t" + gene + "/" + orf + "\t" + gene2 + "/" + orf2 + "\t" + str(x.reference_id) + "\t" + str(pmid) + "\t" + str(sga_score) + "\t" + str(pvalue) + "\t" + type + "\t" + x.description + "\t" + str(x.date_created).split(' ')[0])
                 
 nex_session.close()
 
